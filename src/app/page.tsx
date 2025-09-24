826fd2e2b67e89c6138aec852420d8181275608b
mikek182 <marcosvinicius@geekwork.com.br>
2025-09-23 21:54:51 -0300

    Requisição da API de jogos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+import Container from "@/componentes/container";
+import { GameProps } from "@/utils/types/games";
 import Image from "next/image";
+import Link from "next/link"
 
-export default function Home() {
+async function getDalyGame() {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+    return res.json();
+  } catch (err) {
+    throw new Error("Faliled to fetch data")
+  }
+}
+
+export default async function Home() {
+
+  const daylyGame: GameProps = await getDalyGame()
+
   return (
-    <h1>Home Page</h1>
+    <main className="w-full">
+      <Container>
+        <h1 className="text-center font-bold text-xl text-black mt-8 mb-5">Separamos um jogo exclusivo pra você</h1>
+        <Link href={`/game/${daylyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <Image
+              src={daylyGame.image_url}
+              alt={daylyGame.title}
+              priority={true}
+              quality={100}
+              fill={true}
+            />
+          </section>
+        </Link>
+      </Container>
+    </main >
   );
 }
