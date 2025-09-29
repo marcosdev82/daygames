4241d08da236c793f34849aba9d8c5554210a08a
mikek182 <marcosvinicius@geekwork.com.br>
2025-09-29 11:44:27 -0300

    Criação da páginas de detalhes

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -2,14 +2,24 @@ import Container from "@/componentes/container"
 import { GameProps } from "@/utils/types/games"
 import { redirect } from "next/navigation"
 import Image from "next/image"
+import { Label } from "./components/label"
+import GameCard from "@/componentes/GameCard"
 
 async function getData(id: string) {
-
     try {
-        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`)
+        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`, { next: { revalidate: 60 * 60 * 24 } })
         return res.json();
     } catch (err) {
         return null
+    }
+}
+
+async function getGameSorted() {
+    try {
+        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { cache: 'no-store' })
+        return res.json()
+    } catch (err) {
+        throw new Error("Failed to fetch date")
     }
 }
 
@@ -18,7 +28,7 @@ export default async function Game(props: {
 }) {
     const { id } = await props.params
     const data: GameProps = await getData(id)
-    console.log(data)
+    const sortedGames: GameProps = await getGameSorted();
 
     if (!data) {
         redirect('/')
@@ -40,6 +50,30 @@ export default async function Game(props: {
             <Container>
                 <h1 className="font-bol text-xl my-4">{data.title}</h1>
                 <p>{data.description}</p>
+
+                <h2 className="font-bold text-lg mt-7 mb-2">Categorias:</h2>
+                <div className="flex gap-2 flex-wrap">
+                    {data.categories.map((item) => (
+                        <Label name={item} key={item} />
+                    ))}
+                </div>
+
+                <h2 className="font-bold text-lg mt-7 mb-2">Plataformas:</h2>
+                <div className="flex gap-2 flex-wrap">
+                    {data.platforms.map((item) => (
+                        <Label name={item} key={item} />
+                    ))}
+                </div>
+
+                <p className="mt-7 mb-2">Data de lançamento: {data.realease}</p>
+
+                <h2 className="font-bold text-lg mt-7 mb-2">Jogos recomenddos:</h2>
+                <div className="flex gap-2">
+                    <div className="flex-grow">
+                        <GameCard key={sortedGames.id} data={sortedGames} />
+                    </div>
+                </div>
+
             </Container>
         </main>
     )
